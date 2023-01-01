515ac0ccf26b43baf323cd9f19496ccc6871aee2
Ville Tuulio <ville.tuulio@taiste.com>
2023-01-01 14:11:14 +0200

    fix gh-pages

--- src/App.js
+++ src/App.js
@@ -7,7 +7,6 @@ function App() {
   return (
     <Routes>
       <Route path="/" element={<Main />}/>
-      <Route index element={<Main />} />
       <Route path="/text-inputs" element={<TextInputs />} />
     </Routes>
   );
